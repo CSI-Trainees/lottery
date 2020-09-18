22ce62d1fb8bc4c3f14690cc2d3888baba4c2729
9984995796 <kartikrocks009@gmail.com>
2020-09-19 00:24:17 +0530

    3rd init

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const generateBtn = document.querySelector(".generateBtn");
 
 generateBtn.addEventListener("click", generateRandomNum);
 
+const enterNumBtn = document.querySelector(".enterNumBtn");
+
+enterNumBtn.addEventListener("click", enterRandomNum);
+
 function generateRandomNum(e) {
 
   const toNum = 10;
@@ -13,4 +17,39 @@ function generateRandomNum(e) {
     randomNumEl.innerHTML = randomNum;
     e.stopPropagation();
   
+}
+function enterRandomNum(){
+	var image1= document.getElementById('image1');
+const enterNum = document.querySelector(".enterNum").value;
+num = enterNum ;
+//console.log(enterNum)
+if (num == 20)
+	{
+		image1.src ="images/img1.jfif";
+	}
+	else if (num == 15) {
+		alert(1);
+	}
+	else if (num == 17) {
+		alert(1);
+	}
+	 else {
+	 	alert(2);
+	 }
+
+	
+
+
+
+
+}
+
+
+
+
+document.onkeypress = function(e){
+	e = e || window.event;
+	if(e.keyCode == 13){
+		document.documentElement.classList.toggle('dark-mode');
+	}
 }
